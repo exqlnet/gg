3aa4f079b66a7ed38b31eb7f4c549a47d4439de0
exqlnet <exqlnet@gmail.com>
2022-08-25 15:18:56 +0800

    fix: wrap

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/exqlnet/gg
+
+go 1.17
+
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539
+)
--- wrap.go
+++ wrap.go
@@ -15,7 +15,7 @@ func splitOnSpace(x string) []string {
 	ps := false
 	for i, c := range x {
 		s := unicode.IsSpace(c)
-		if (s != ps && i > 0) || unicode.Is(unicode.Han, c) {
+		if (s != ps || unicode.Is(unicode.Han, c) || (i-pi) >= 20) && i > 0 {
 			result = append(result, x[pi:i])
 			pi = i
 		}
@@ -27,7 +27,7 @@ func splitOnSpace(x string) []string {
 
 func splitRune(x string) []string {
 	var result []string
-	
+
 	for _, c := range x {
 		result = append(result, string(c))
 	}
@@ -38,23 +38,19 @@ func wordWrap(m measureStringer, s string, width float64) []string {
 	var result []string
 	for _, line := range strings.Split(s, "\n") {
 		fields := splitOnSpace(line)
-		if len(fields)%2 == 1 {
-			fields = append(fields, "")
-		}
 		x := ""
-		for i := 0; i < len(fields); i += 2 {
+		for i := range fields {
 			w, _ := m.MeasureString(x + fields[i])
 			if w > width {
 				if x == "" {
 					result = append(result, fields[i])
-					x = ""
 					continue
 				} else {
 					result = append(result, x)
 					x = ""
 				}
 			}
-			x += fields[i] + fields[i+1]
+			x += fields[i]
 		}
 		if x != "" {
 			result = append(result, x)
